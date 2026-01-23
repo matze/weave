f5279b57e93ebc56252bb68d08ff69dc7a74b81a
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2026-01-23 22:55:54 +0100

    Use absolute path for CSS asset

--- src/partials/head.rs
+++ src/partials/head.rs
@@ -5,7 +5,7 @@ pub(crate) fn head() -> Markup {
         head {
             meta charset="utf-8";
             meta name="viewport" content="width=device-width, initial-scale=1.0";
-            link rel="stylesheet" type="text/css" href="app.css";
+            link rel="stylesheet" type="text/css" href="/app.css";
             title {"weave"};
             script {
                 (maud::PreEscaped(r#"
