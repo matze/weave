03ef4f798199776a4e06a7bc826cee4d19577f85
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2026-03-08 19:56:58 +0100

    Fix font loading

--- crates/weave/src/partials/head.rs
+++ crates/weave/src/partials/head.rs
@@ -7,6 +7,9 @@ pub(crate) fn head() -> Markup {
             meta charset="utf-8";
             meta name="viewport" content="width=device-width, initial-scale=1.0";
             meta name="htmx-config" content=r#"{"refreshOnHistoryMiss":true}"#;
+            link rel="preconnect" href="https://fonts.googleapis.com";
+            link rel="preconnect" href="https://fonts.gstatic.com" crossorigin;
+            link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,100..900;1,100..900&display=swap";
             link rel="stylesheet" type="text/css" href="/app.css";
             link rel="stylesheet" type="text/css" href="/highlight.css";
             link rel="shortcut icon" type="image/svg+xml" href="/favicon.svg";
